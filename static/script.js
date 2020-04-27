918b0581f9720a4e18bfd767d324abbe8071fcea
jpnaterer <jordan.naterer@gmail.com>
2020-04-26 18:43:09 -0700

    Bandcamp genre toggle button.

--- static/script.js
+++ static/script.js
@@ -1,16 +1,31 @@
+$(document).ready(function() {
+
+$( ".toggle-genre-tor" ).click(function() {
+    $( ".genre-tor" ).toggle( "slow", function() {});
+    
+    var text = $('.toggle-genre-tor').text();
+    $('.toggle-genre-tor').text(
+        text == "Show Genres" ? "Hide Genres" : "Show Genres");
+});
+
+$( ".toggle-genre-mon" ).click(function() {
+    $( ".genre-mon" ).toggle( "slow", function() {});
+    
+    var text = $('.toggle-genre-mon').text();
+    $('.toggle-genre-mon').text(
+        text == "Show Genres" ? "Hide Genres" : "Show Genres");
+});
+
+$( ".toggle-genre-van" ).click(function() {
+    $( ".genre-van" ).toggle( "slow", function() {});
+    
+    var text = $('.toggle-genre-van').text();
+    $('.toggle-genre-van').text(
+        text == "Show Genres" ? "Hide Genres" : "Show Genres");
+});
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
+});
 
-/*
-<script type="text/javascript" charset="utf8" 
-    src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
-
-$(document).ready( function () {
-    $('#sb_table').DataTable( {
-        paging: false,
-        "searching": false,
-        "info": false,
-        "order": []
-    } );
-} );*/
+});
